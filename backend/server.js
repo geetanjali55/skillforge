7e437a5c02dd41337ffdacc5ed015a74374638a9
Geetanjali55 <abhinavmahajan1108@gmail.com>
2026-04-20 21:14:35 +0530

    basic server setup done

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+const app = express();
+const PORT = 4000 ;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
